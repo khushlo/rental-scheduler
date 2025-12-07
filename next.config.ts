93bf79e431e431d79c462324bd42304805f1f5a5
khushlo <kdp0009@gmail.com>
2025-12-08 00:19:09 +0530

    Azure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  poweredByHeader: false,
+  compress: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
 };
 
 export default nextConfig;
